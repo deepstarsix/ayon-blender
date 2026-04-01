5b117e5007cc647beabec89378910b75432bb315
Vicko <59090367+deepstarsix@users.noreply.github.com>
2026-04-01 08:27:29 +0200

    ver

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "blender"
 title = "Blender"
-version = "1.1.1+dev"
+version = "1.1.1+poster"
 app_host_name = "blender"
 client_dir = "ayon_blender"
 project_can_override_addon_version = True
